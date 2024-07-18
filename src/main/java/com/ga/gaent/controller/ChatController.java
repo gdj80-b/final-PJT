20a20746ed78195d1312acb063a6bd9e766d484f
Geonhee <164113310+Geonhee-J@users.noreply.github.com>
2024-07-19 01:41:30 +0900

    정건희[feat]: 채팅 구독 경로 테스트
    1. 구독 경로를 전체를 설정
    2. 다른 브라우저 크롬, 엣지에서 메세지 출력 확인
    3. 메시지 내용 null 또는 undefined 부분 수정 필요
    4. 특정 대상자 지정하는 구독 경로 설정 필요

--- src/main/java/com/ga/gaent/controller/ChatController.java
+++ src/main/java/com/ga/gaent/controller/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.ga.gaent.dto.ChatDTO;
@@ -14,33 +15,48 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class ChatController {
-    
+
     @Autowired private SimpMessagingTemplate simpMessagingTemplate;
 
     @GetMapping("/chat")
     public String chat() {
         return "chat/chat";
     }
-    
-    @GetMapping("/testChat")
-    public String testChat() {
-        return "chat/testChat";
+
+    @GetMapping("/bubble")
+    public String speechBubble() {
+        return "chat/speechBubble";
     }
-    
+
+    /*
+     * @MessageMapping("/chat.sendMessage")
+     * 
+     * @SendToUser("/queue/messages") public void sendMessage(@Payload ChatDTO chatDTO) { if
+     * (chatDTO.getType() == MessageType.CHAT) { log.debug(TeamColor.BLUE_BG + "CHAT.TYPE: " + chatDTO +
+     * TeamColor.RESET); simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(),
+     * "/queue/messages", chatDTO); } else if (chatDTO.getType() == MessageType.JOIN) {
+     * log.debug(TeamColor.PURPLE_BG + "JOIN.TYPE: " + chatDTO + TeamColor.RESET);
+     * chatDTO.setMessage(chatDTO.getSender() + " joined the chat");
+     * simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(), "/queue/messages", chatDTO); }
+     * else if (chatDTO.getType() == MessageType.LEAVE) { log.debug(TeamColor.RED_BG + "LEAVE.TYPE: " +
+     * chatDTO + TeamColor.RESET); chatDTO.setMessage(chatDTO.getSender() + " left the chat");
+     * simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(), "/queue/messages", chatDTO); }
+     * }
+     */
+
     @MessageMapping("/chat.sendMessage")
     public void sendMessage(@Payload ChatDTO chatDTO) {
         if (chatDTO.getType() == MessageType.CHAT) {
             log.debug(TeamColor.BLUE_BG + "CHAT.TYPE: " + chatDTO + TeamColor.RESET);
-            simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(), "/queue/messages", chatDTO);
+            simpMessagingTemplate.convertAndSend("/queue/messages", chatDTO);
         } else if (chatDTO.getType() == MessageType.JOIN) {
             log.debug(TeamColor.PURPLE_BG + "JOIN.TYPE: " + chatDTO + TeamColor.RESET);
             chatDTO.setMessage(chatDTO.getSender() + " joined the chat");
-            simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(), "/queue/messages", chatDTO);
+            simpMessagingTemplate.convertAndSend("/queue/messages", chatDTO);
         } else if (chatDTO.getType() == MessageType.LEAVE) {
             log.debug(TeamColor.RED_BG + "LEAVE.TYPE: " + chatDTO + TeamColor.RESET);
             chatDTO.setMessage(chatDTO.getSender() + " left the chat");
-            simpMessagingTemplate.convertAndSendToUser(chatDTO.getReceiver(), "/queue/messages", chatDTO);
+            simpMessagingTemplate.convertAndSend("/queue/messages", chatDTO);
         }
     }
 }
- 
